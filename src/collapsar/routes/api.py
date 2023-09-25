86fa038eba3b2d56c0ec8b44ddef3ff31a1141d2
Eduardo Aguad <eduaguad@gmail.com>
2023-09-25 16:18:46 -0300

    replace string routes with explicit methods

--- src/collapsar/routes/api.py
+++ src/collapsar/routes/api.py
@@ -1,15 +1,21 @@
 from masonite.routes import Route
+from ..controllers.ResourceStoreController import ResourceStoreController
+from ..controllers.ResourceIndexController import ResourceIndexController
+from ..controllers.FieldsController import FieldsController
+from ..controllers.ResourceShowController import ResourceShowController
+from ..controllers.ResourceUpdateController import ResourceUpdateController
+from ..controllers.ResourceDeleteController import ResourceDeleteController
 
 ROUTES = [
     Route.group(
         [
-            Route.post("/@resource", "ResourceStoreController@handle"),
-            Route.get("/@resource", "ResourceIndexController@handle"),
-            Route.get("/@resource/creation-fields", "FieldsController@creation"),
-            Route.get("/@resource/@resource_id", "ResourceShowController@handle"),
-            Route.patch("/@resource/@resource_id", "ResourceUpdateController@handle"),
-            Route.delete("/@resource/@resource_id", "ResourceDeleteController@handle"),
-            Route.put("/@resource/", "ResourceStoreController@handle"),
+            Route.post("/@resource", ResourceStoreController.handle),
+            Route.get("/@resource", ResourceIndexController.handle),
+            Route.get("/@resource/creation-fields", FieldsController.creation),
+            Route.get("/@resource/@resource_id", ResourceShowController.handle),
+            Route.patch("/@resource/@resource_id", ResourceUpdateController.handle),
+            Route.delete("/@resource/@resource_id", ResourceDeleteController.handle),
+            Route.put("/@resource/", ResourceStoreController.handle),
         ],
         prefix="/collapsar/api",
     )
--- src/collapsar/routes/web.py
+++ src/collapsar/routes/web.py
@@ -1,12 +1,13 @@
 from masonite.routes import Route
+from ..controllers.CollapsarController import CollapsarController
 
 ROUTES = [
     Route.group(
         [
-            Route.get("/", "CollapsarController@index"),
-            Route.get("/resource/@urikey", "CollapsarController@index"),
-            Route.get("/resource/@urikey/@id", "CollapsarController@index"),
-            Route.get("/resource/@urikey/@id/edit", "CollapsarController@index"),
+            Route.get("/", CollapsarController.index),
+            Route.get("/resource/@urikey", CollapsarController.index),
+            Route.get("/resource/@urikey/@id", CollapsarController.index),
+            Route.get("/resource/@urikey/@id/edit", CollapsarController.index),
         ],
         prefix="/collapsar",
     )
