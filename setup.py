0fe3199a0734642e8ebb97e915df83ddf71a9a77
Eduardo Aguad <eduaguad@gmail.com>
2023-09-25 13:21:31 -0300

    fix templates folder

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        "": ["templates/*.html", "dist/**"],
+        "": ["templates/**", "dist/**"],
     },
 )
